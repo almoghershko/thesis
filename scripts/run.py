baa37d959225a9f94229f8bdc514c7d513152484
Almog Hershko <almog.hershko@gmail.com>
2022-01-11 17:04:40 +0200

    added a scripts dir and deleted some trash

---
+++ scripts/run.py
@@ -0,0 +1,44 @@
+# imports
+import pandas as pd
+import numpy as np
+import os
+import sys
+import pickle
+from matplotlib import pyplot as plt
+import matplotlib
+import boto3
+
+# random seed
+seed = 42
+np.random.seed(seed)
+
+# local files paths
+local_home_dir_path = os.path.expanduser("~")
+local_work_dir_path = os.path.join(local_home_dir_path, 'thesis')
+local_code_dir_path = os.path.join(local_work_dir_path , 'code')
+
+# S3 file paths
+endpoint_url = 'https://s3.nautilus.optiputer.net'
+bucket_name = 'tau-astro'
+prefix = 'almogh'
+s3_work_dir_path = os.path.join(prefix, 'workdir3')
+s3_saves_dir_path = os.path.join(s3_work_dir_path , 'model_saves')
+s3_data_dir_path = os.path.join(s3_work_dir_path , 'data')
+s3_data_ver_dir_path = os.path.join(s3_data_dir_path,'HighSNR_12K_V1')
+s3_runs_dir_path = os.path.join(s3_work_dir_path , 'runs')
+
+s3_client = boto3.client("s3", endpoint_url=endpoint_url)
+
+# adding code folder to path
+sys.path.insert(1, local_code_dir_path)
+from s3 import to_s3_npy, to_s3_pkl, from_s3_npy, from_s3_pkl, to_s3_fig
+
+import sys
+run_name = sys.argv[1]
+s3_run_dir_path = os.path.join(s3_runs_dir_path, run_name)
+print('run dir path = {0}'.format(s3_run_dir_path))
+
+import GPUtil
+print('GPUs:\n{0}'.format('\n'.join(['('+str(i+1)+')\t'+gpu.name for i,gpu in enumerate(GPUtil.getGPUs())])))
+
+to_s3_npy(np.array([0]), s3_client, bucket_name, os.path.join(s3_run_dir_path, 'test.npy'))
